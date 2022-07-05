5ca4724bf45791f7e792c5cf0786d6dd406623dd
godsakani <nyonggodwill11@gmail.com>
2022-07-05 19:47:46 +0100

    file: added About component

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <div>
+        <h2>Trusted Developers across the world</h2>
+        <p>In the first part, I will show you how to build a chat application, 
+        where users can sign up and talk to other users in a single chat room. The exchange of messages will happen in real time,
+         meaning that you do not have to refresh the page to load new messages.</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
