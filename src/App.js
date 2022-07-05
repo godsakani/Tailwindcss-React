eb178160d88af547276cea4a35bf7830b599b1a9
godsakani <nyonggodwill11@gmail.com>
2022-07-05 19:44:10 +0100

    feat: import hero component import About section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import About from "./components/About";
+import Hero from "./components/hero";
 import Navbar from "./components/navbar";
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
+      <About />
     </>
   );
 }
